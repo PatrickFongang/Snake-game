016d13c35e9239e251bf4821d509353850460ae2
LegalnyPoeta <pfongang2005@gmail.com>
2025-11-06 17:52:07 +0100

    small bug fix

--- Snake_csharp/MainWindow.xaml.cs
+++ Snake_csharp/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Snake_csharp;
+
 
 namespace Snake_csharp
 {
@@ -272,6 +264,7 @@ namespace Snake_csharp
         private async Task ShowGameOver()
         {
             await DrawDeadSnake();
+            GameOverText.Text = "GAME OVER";
             SetHighScore();
             ShowGameOverScreen();
         }
